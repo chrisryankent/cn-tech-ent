a79096bf237378bebd58d2e102363ebd80842e9b
chris kent <142613652+chrisryankent@users.noreply.github.com>
2025-01-18 03:23:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -135,4 +135,4 @@ def fetch_related_videos(video_title, video_owner):
     return data.get('items', [])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
